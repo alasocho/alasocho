37f6394b0a57b52b4f5a425db3c8a4e2c916e80f
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2011-08-12 23:31:22 -0300

    Add a... um, home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def current_locale
+    I18n.locale
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ALasOcho::Application.routes.draw do
+  root to: "pages#home"
+
   resources :events, :only => [:new, :create, :show]
 
   match "/auth/:provider/callback" => "sessions#create"
