1404e8cb43cb64a36c5e159155fffe1f26a264be
Nacho Facello <nacho@nucleartesuji.com>
2011-08-13 09:13:31 -0300

    Add Attendance#token

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110813070213) do
+ActiveRecord::Schema.define(:version => 20110813121039) do
 
   create_table "attendances", :force => true do |t|
     t.integer  "user_id"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(:version => 20110813070213) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "email"
+    t.string   "token"
   end
 
   create_table "authorizations", :force => true do |t|
