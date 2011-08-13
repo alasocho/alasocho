2a2da87f2cc71893f8852ffc34b243d312001a7b
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2011-08-12 21:48:10 -0300

    Add User model

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110813003015) do
+ActiveRecord::Schema.define(:version => 20110813004718) do
 
   create_table "attendances", :force => true do |t|
     t.integer  "user_id"
@@ -41,6 +42,13 @@ ActiveRecord::Schema.define(:version => 20110813003015) do
     t.string   "state"
     t.integer  "attendee_quota"
     t.integer  "host_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "display_name"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
