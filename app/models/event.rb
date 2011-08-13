e30197d84669e7f41a8a16a4b95619f3c1c6f4e3
Nacho Facello <nacho@nucleartesuji.com>
2011-08-12 22:49:43 -0300

    Rage in favor of the State machines

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,19 @@
+require 'micromachine'
+
 class Event < ActiveRecord::Base
   has_many :attendances
   has_many :comments
+
+  before_save :preserve_state_machine
+
+  def state_machine
+    @state_machine ||= MicroMachine.new(state || "created").tap do |machine|
+      machine.transitions_for[:publish] = { "created" => "published" }
+      machine.transitions_for[:cancel]  = { "created" => "cancelled", "published" => "cancelled" }
+    end
+  end
+
+  def preserve_state_machine
+    self.state = state_machine.state
+  end
 end
