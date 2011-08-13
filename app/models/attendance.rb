30feeba3bfec96fb65c683ed063511c384e5313d
Nacho Facello <nacho@nucleartesuji.com>
2011-08-12 22:23:23 -0300

    Scope for Attendances that need_attention (for notifications)

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,4 +1,9 @@
 class Attendance < ActiveRecord::Base
   belongs_to :event
   belongs_to :user
+
+  STATES_NEEDING_ACTION = %w(invited tentative)
+  STATES_INTERESTED     = %w(confirmed tentative waitlisted)
+
+  scope :need_attention, joins(:event).where("attendances.state in (?) OR (attendances.state IN (?) AND events.last_commented_at > attendances.updated_at)", STATES_NEEDING_ACTION, STATES_INTERESTED)
 end
