90d42c4c5f08b4970d0e6903f3ef2d6da7159720
Nacho Facello <nacho@nucleartesuji.com>
2011-08-13 02:17:45 -0300

    Fix the email.

--- app/mailers/attendance_mailer.rb
+++ app/mailers/attendance_mailer.rb
@@ -1,8 +1,10 @@
 class AttendanceMailer < ActionMailer::Base
   def invite_notification(attendance)
+    @attendance = attendance
     mail(
       :subject => t("email.attendance.invite.subject", :event_name => attendance.event.name),
-      :to      => attendance.email
+      :to      => attendance.email,
+      :from    => "arturito@alasocho.com"
     )
   end
 end
