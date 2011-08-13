09dfdcb299d7c7315d937afb1637ef40a13f3de0
Bruno Aguirre <yo@brunoaguirre.com>
2011-08-12 21:07:42 -0300

    Capistrano script

---
+++ config/deploy.rb
@@ -0,0 +1,70 @@
+set :application,               "alasocho"
+set :repository,                "git@github.com:rallyonrails/2011-equipo-09.git"
+set :use_sudo,                  false
+
+set :deploy_to,                 "/data/#{application}"
+set :deploy_via,                :remote_cache
+set :user,                      "deploy"
+set :normalize_asset_timestamps, false
+set :scm,                       "git"
+
+ssh_options[:forward_agent] = true
+
+task :production do
+  set :rails_env, "production"
+  set :branch, "master"
+
+  role :app, 'alasocho.com'
+  role :web, 'alasocho.com'
+  role :db,  'alasocho.com', primary: true
+end
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, roles: :app, except: { no_release: true } do
+    run "touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+namespace :bundler do
+  task :create_symlink, :roles => :app do
+    shared_dir = File.join(shared_path, 'bundle')
+    release_dir = File.join(release_path, 'vendor', 'bundle')
+    run("mkdir -p #{shared_dir} && ln -s #{shared_dir} #{release_dir}")
+  end
+
+  task :install, :roles => :app do
+    run "cd #{release_path} && bundle install --without 'development test' --deployment"
+
+    on_rollback do
+      if previous_release
+        run "cd #{previous_release} && bundle install --without 'development test' --deployment"
+      else
+        logger.important "no previous release to rollback to, rollback of bundler:install skipped"
+      end
+    end
+  end
+
+  task :bundle_new_release, roles: [:app] do
+    bundler.create_symlink
+    bundler.install
+  end
+end
+
+task :restart_resque do
+  run "cd #{current_path} && RAILS_ENV=#{rails_env} VERBOSE=1 QUEUE=* ./script/resque_worker restart"
+end
+
+task :compile_coffesscript do
+  run "cd #{current_path} && RAILS_ENV=#{rails_env} rake barista:brew"
+end
+
+task :create_various_symlinks do
+  run "ln -s #{shared_path}/database.yml #{release_path}/config/database.yml"
+end
+
+after "deploy:rollback:revision", "bundler:install"
+after "deploy:update_code", "bundler:bundle_new_release"
+after "deploy:update_code", "create_various_symlinks"
+after "deploy:restart", "restart_resque"
