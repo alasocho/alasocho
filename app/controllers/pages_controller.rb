a7b8c6eca19bba6816e580991c0561d4b4f3118f
Pablo Astigarraga <pote@cuboxsa.com>
2011-08-13 19:32:26 -0300

    added public events to dashboard, internalization fixed, more or less.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def home
     @my_events = current_user.interests.order("start_at DESC").limit(10)
+    @public_events = Event.where(:public => true).limit(15)
   end
 end
