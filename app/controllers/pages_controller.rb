e81a47fc855433000c2c7bd30e0ebcf56243519a
Nacho Facello <nacho@nucleartesuji.com>
2011-08-14 20:03:40 -0300

    Near my city

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def home
-    @public_events = Event.public_events.limit(4)
+    @current_city = GeoLocator.city_from_ip(request.remote_addr)
+    @public_events = Event.public_events_near(@current_city).limit(4)
     redirect_to user_root_path if signed_in?
   end
 end
