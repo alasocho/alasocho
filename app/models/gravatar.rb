b93bcf6ec4999ad5f7e062ca916c8df2d2bbd832
Pablo Astigarraga <pote@cuboxsa.com>
2011-08-14 15:05:50 -0300

    Added pretty default avatars!

--- app/models/gravatar.rb
+++ app/models/gravatar.rb
@@ -5,6 +5,11 @@ class Gravatar
 
   def path(options = {})
     options[:size] ||= 96
-    "http://www.gravatar.com/avatar/#{Digest::MD5::hexdigest(@email.downcase)}?s=#{options[:size]}&d=monsterid"
+    "http://www.gravatar.com/avatar/#{Digest::MD5::hexdigest(@email.downcase)}?s=#{options[:size]}&d=#{ default_avatar }"
+  end
+
+  def default_avatar
+    hash = @email.hash % 5
+    "http://#{ ALasOcho.config[:canonical_host] }/assets/default_avatars/#{ hash }.jpg"
   end
 end
