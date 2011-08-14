bdce01ba660ef2e9c0f4351288319ee90a261fb2
Nacho Facello <nacho@nucleartesuji.com>
2011-08-14 20:24:24 -0300

    Limits

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,9 +4,9 @@ class DashboardController < ApplicationController
   def index
     @page_title = t("dashboard.title")
 
-    @my_events = current_user.interests.order("start_at DESC").limit(10)
+    @my_events = current_user.interests.future.order("start_at DESC").limit(10)
     @current_city = GeoLocator.city_from_ip(request.remote_addr)
-    @public_events = Event.public_events_near(@current_city).limit(5)
+    @public_events = Event.public_events_near(@current_city).limit(5).future
     @pending_invites = current_user.pending_attendances.joins(:event).order("events.start_at DESC").limit(5)
     render "dashboard/dashboard"
   end
