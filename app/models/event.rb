2ae504a256da6ae0c93979454738d8b572b8aad1
Nacho Facello <nacho@nucleartesuji.com>
2011-08-14 10:12:53 -0300

    Comments in sensible order

--- app/models/event.rb
+++ app/models/event.rb
@@ -17,7 +17,7 @@ class Event < ActiveRecord::Base
   has_many :interested_invitations, :class_name => "Attendance", :conditions => { :state => Attendance::STATES_INTERESTED }
   has_many :interested, :through => :interested_invitations, :source => :user
 
-  has_many :comments
+  has_many :comments, :order => "created_at desc"
   belongs_to :host, :class_name => "User"
 
   before_save :preserve_state_machine
