47c5d7fe610b581602539ca308ac4ca3cd00aec8
Nacho Facello <nacho@nucleartesuji.com>
2011-08-14 08:53:16 -0300

    Render Attendances, not Users

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -68,11 +68,11 @@ class EventsController < ApplicationController
 
   def show
     load_event
-    @attendance          = @event.attendance_for(current_user).tap { |a| a.touch(:updated_at) unless a.new_record? }
-    @comments            = @event.comments
-    @confirmed_attendees = @event.confirmed_invitations.includes(:user).limit(MAX_CONFIRMED_ATTENDEES).map(&:owner)
-    @waitlisted          = @event.waitlisted_invitations.includes(:user).limit(MAX_WAITLISTED_ATTENDEES).map(&:owner)
-    @invited             = @event.pending_invitations.includes(:user).limit(MAX_PENDING_ATTENDEES).map(&:owner)
+    @attendance             = @event.attendance_for(current_user).tap { |a| a.touch(:updated_at) unless a.new_record? }
+    @comments               = @event.comments
+    @confirmed_invitations  = @event.confirmed_invitations.includes(:user).limit(MAX_CONFIRMED_ATTENDEES)
+    @waitlisted_invitations = @event.waitlisted_invitations.includes(:user).limit(MAX_WAITLISTED_ATTENDEES)
+    @invited_invitations    = @event.pending_invitations.includes(:user).limit(MAX_PENDING_ATTENDEES)
   end
 
   def public
