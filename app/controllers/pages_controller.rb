09b93c54dc9a63fd824e67161d56fc72adc7a2eb
Nacho Facello <nacho@nucleartesuji.com>
2011-08-14 20:03:03 -0300

    Reasonabler limit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
-    @public_events = Event.public_events.limit(10)
+    @public_events = Event.public_events.limit(4)
     redirect_to user_root_path if signed_in?
   end
 end
