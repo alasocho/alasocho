5a30f8546a90a5921c971a6c7d3251aa916a8ef4
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2011-08-18 15:04:46 -0300

    Fix the test helper to create events

--- test/integration/create_event_test.rb
+++ test/integration/create_event_test.rb
@@ -7,7 +7,7 @@ class CreateEventTest < ActionDispatch::IntegrationTest
     sign_in :event_host
 
     click_link t("events.form.button.new")
-    create_event name: "My Event"
+    create_event name: "My Event", end_at: 3.hours.from_now
     click_button t("event.form.invite.submit") # FIXME: Use a better label
 
     event = Event.find_by_name("My Event")
--- test/integration_test_helper.rb
+++ test/integration_test_helper.rb
@@ -33,9 +33,9 @@ class ActionDispatch::IntegrationTest
     with_options scope: "activerecord.attributes.event" do |label|
       fill_in label.t(:name),          with: attrs.fetch(:name)
       fill_in label.t(:description),   with: attrs.fetch(:description)
-      fill_in "event[start_at][date]", with: 2.hours.from_now.strftime("%Y-%m-%d")
-      fill_in "event[start_at][time]", with: 2.hours.from_now.strftime("%H-%M")
-      fill_in "event[end_at][date]",   with: attrs.fetch(:start_at).strftime("%Y-%m-%d")
+      fill_in "event[start_at][date]", with: attrs.fetch(:start_at).strftime("%Y-%m-%d")
+      fill_in "event[start_at][time]", with: attrs.fetch(:start_at).strftime("%H-%M")
+      fill_in "event[end_at][date]",   with: attrs.fetch(:end_at).strftime("%Y-%m-%d")
       fill_in "event[end_at][time]",   with: attrs.fetch(:end_at).strftime("%H-%M")
       fill_in label.t(:location),      with: attrs.fetch(:location)
       fill_in label.t(:city),          with: attrs.fetch(:city)
