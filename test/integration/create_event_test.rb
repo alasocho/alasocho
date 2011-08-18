5aa80f6a57a2de65e20894318dd42f6441509adf
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2011-08-18 14:59:36 -0300

    Test for creating and publishing an event (#18)
    Make sure Filling in the form and publishing an event leaves the event
    published, and that it doesn't set +end_at+ to NULL.

---
+++ test/integration/create_event_test.rb
@@ -0,0 +1,17 @@
+require 'integration_test_helper'
+
+class CreateEventTest < ActionDispatch::IntegrationTest
+  fixtures :all
+
+  test "create an event and publish it" do
+    sign_in :event_host
+
+    click_link t("events.form.button.new")
+    create_event name: "My Event"
+    click_button t("event.form.invite.submit") # FIXME: Use a better label
+
+    event = Event.find_by_name("My Event")
+    assert_equal "published", event.state
+    assert event.end_at.present?
+  end
+end
--- test/integration_test_helper.rb
+++ test/integration_test_helper.rb
@@ -1,6 +1,50 @@
 require "test_helper"
 require "capybara/rails"
 
+OmniAuth.configure do |config|
+  config.test_mode = true
+  config.add_mock(:google, uid: "john.doe@google.com")
+end
+
 class ActionDispatch::IntegrationTest
   include Capybara
+
+  def sign_in(user)
+    user = User === user ? user : users(user)
+    auth = user.authorizations.first
+    visit "/auth/#{auth.provider}"
+  end
+
+  def sign_out
+    visit "/auth/sign_out"
+  end
+
+  # Fill in and submit the first step of creating an event.
+  def create_event(attrs={})
+    attrs.reverse_merge!(
+      name: "The Event",
+      description: "The Description",
+      start_at: 2.hours.from_now,
+      end_at: 3.hours.from_now,
+      location: "The Location",
+      city: "Montevideo, Uruguay"
+    )
+
+    with_options scope: "activerecord.attributes.event" do |label|
+      fill_in label.t(:name),          with: attrs.fetch(:name)
+      fill_in label.t(:description),   with: attrs.fetch(:description)
+      fill_in "event[start_at][date]", with: 2.hours.from_now.strftime("%Y-%m-%d")
+      fill_in "event[start_at][time]", with: 2.hours.from_now.strftime("%H-%M")
+      fill_in "event[end_at][date]",   with: attrs.fetch(:start_at).strftime("%Y-%m-%d")
+      fill_in "event[end_at][time]",   with: attrs.fetch(:end_at).strftime("%H-%M")
+      fill_in label.t(:location),      with: attrs.fetch(:location)
+      fill_in label.t(:city),          with: attrs.fetch(:city)
+    end
+
+    click_button t("helpers.submit.create", model: t("activerecord.models.event"))
+  end
+
+  def t(key, options={})
+    I18n.t(key, options)
+  end
 end
