fe05cd713d23df9d241f328c3031d12f6843e07f
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2011-08-19 23:30:37 -0300

    Clean up the GeoLocator model

--- app/models/geo_locator.rb
+++ app/models/geo_locator.rb
@@ -1,22 +1,43 @@
 require 'geoip'
+require 'ostruct'
 
 class GeoLocator
-  def self.data(ip)
-    source.city(ip)
-  end
-
-  def self.source
-    @source ||= GeoIP.new(file)
-  end
-
-  def self.file
-    Rails.root.join("data", "GeoLiteCity.dat")
-  end
-
   def self.city_from_ip(ip)
+    ActiveSupport::Deprecation.warn("GeoLocator.city_from_ip is deprecated. Use GeoLocator#location instead")
     return "Montevideo, Uruguay" if Rails.env.development?
-    info = data(ip)
-    return "" if info.blank?
-    [:city_name, :country_name].map { |m| info.send(m) }.compact.join(", ").encode("UTF-8")
+    new(ip).location
+  end
+
+  cattr_accessor :default_source
+  self.default_source = GeoIP.new(Rails.root.join("data", "GeoLiteCity.dat"))
+
+  @@mock = OpenStruct.new(city_name: "Montevideo",
+                          country_name: "Uruguay",
+                          timezone: "America/Montevideo")
+
+  def initialize(ip, source=self.class.default_source)
+    @ip = ip
+
+    if @ip == "127.0.0.1"
+      @data = @@mock
+    else
+      @data = source.city(@ip)
+    end
+  end
+
+  def city
+    @data.city_name
+  end
+
+  def country
+    @data.country_name
+  end
+
+  def timezone
+    @data.timezone
+  end
+
+  def to_s
+    [city, country].compact.join(", ")
   end
 end
