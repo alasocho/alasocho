1338fa3c7014c90bb6d8c919f5c131146968f87e
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2011-09-19 00:10:41 -0300

    Use foca/alasocho for deployments

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,9 @@
 $:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
-require "rvm/capistrano"                  # Load RVM's capistrano plugin.
-set :rvm_ruby_string, '1.9.2'        # Or whatever env you want it to run in.
+require "rvm/capistrano"      # Load RVM's capistrano plugin.
+set :rvm_ruby_string, '1.9.2' # Or whatever env you want it to run in.
 
 set :application,               "alasocho"
-set :repository,                "git@github.com:rallyonrails/2011-equipo-09.git"
+set :repository,                "git@github.com:foca/alasocho.git"
 set :use_sudo,                  false
 
 set :deploy_to,                 "/data/#{application}"
