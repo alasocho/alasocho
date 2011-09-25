3cf0552fc6d709655ab0d34b2c5999107eb60638
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2011-09-25 04:21:11 -0300

    Test Timezone#to_json

--- spec/unit/timezone_spec.rb
+++ spec/unit/timezone_spec.rb
@@ -1,5 +1,6 @@
 require "unit_spec_helper"
 require "timezone"
+require "json"
 
 describe Timezone do
   subject { Timezone.get("America/New_York") }
@@ -8,6 +9,28 @@ describe Timezone do
   its(:name)       { should == "America - New York" }
   its(:code)       { should == :EDT }
   its(:offset)     { should == -4 }
+
+  describe "#to_json" do
+    let :json_attributes do
+      JSON.parse(subject.to_json)
+    end
+
+    it "contains the name" do
+      json_attributes["name"].should == subject.name.to_s
+    end
+
+    it "contains the code" do
+      json_attributes["code"].should == subject.code.to_s
+    end
+
+    it "contains the offset" do
+      json_attributes["offset"].should == subject.offset.to_i
+    end
+
+    it "contains the identier" do
+      json_attributes["identifier"].should == subject.identifier.to_s
+    end
+  end
 
   describe Timezone::Unidentified do
     subject { Timezone::Unidentified.new }
