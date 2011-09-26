47be47e6245fc2bb79a48439c056c960a8bbbf3d
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2011-09-26 15:38:33 -0300

    Add a test for inviting people while creating an event

--- spec/acceptance/organize_event_spec.rb
+++ spec/acceptance/organize_event_spec.rb
@@ -15,4 +15,20 @@ describe "User organizes an event" do
     event.state.should == "published"
     event.start_at.should be_present
   end
+
+  it "can invite people when organizing an event", :js do
+    sign_in :event_host
+
+    click_link t("events.form.button.new")
+    create_event name: "My Event", end_at: 3.hours.from_now
+
+    fill_in t("events.invitation_box.label"), with: "invitee1@example.com, invitee2@example.com"
+    click_button t("event.form.invite.submit") # FIXME: Use a better label
+
+    within "#guests .pending" do
+      page.should have_content(t("guests.invited", count: 2))
+      page.should have_content("invitee1")
+      page.should have_content("invitee2")
+    end
+  end
 end
