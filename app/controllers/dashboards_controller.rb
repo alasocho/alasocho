33008198e298d1c2a32e7bb4b36097408c73de64
PoTe <poteland@gmail.com>
2011-09-26 13:40:57 -0300

    My events were being shown in the wrong order, the ones closest in time should be first

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,7 +4,7 @@ class DashboardsController < ApplicationController
   def show
     @page_title = t("dashboard.title")
 
-    @my_events = current_user.interests.future.order("start_at DESC").limit(10)
+    @my_events = current_user.interests.future.order("start_at ASC").limit(10)
     @public_events = Event.public_events_near(current_location.to_s).limit(5).future
     @pending_invites = current_user.pending_attendances.joins(:event).order("events.start_at DESC").limit(5)
   end
